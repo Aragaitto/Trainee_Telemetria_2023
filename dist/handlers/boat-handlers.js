c2a918b61e2c153910a797e84d7d1ecc7f74d0de
andreocunha <andreoliveiracunha20@gmail.com>
2023-05-10 23:50:18 -0300

    setInterval problem

--- dist/handlers/boat-handlers.js
+++ dist/handlers/boat-handlers.js
@@ -1,13 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handleBoat = void 0;
-const dataProcessing_1 = require("../utils/dataProcessing");
-function handleBoat(socket, io, speed, coordenadas, coord) {
-    // console.log(data);
-    let newData = (0, dataProcessing_1.convertData)(coordenadas, coord);
-    //newData.velocidadeBarco = speed[0];
-    //console.log(newData)
-    io.emit("info", newData);
+function handleBoat(socket, io, speed, coordenadas) {
     socket.on("speed", (data) => {
         speed.pop();
         speed.push(data);
--- dist/server/socket.js
+++ dist/server/socket.js
@@ -4,6 +4,7 @@ exports.configureSockets = void 0;
 const boat_handlers_1 = require("../handlers/boat-handlers");
 const history_handlers_1 = require("../handlers/history-handlers");
 const setCoordenates_1 = require("../utils/setCoordenates");
+const dataProcessing_1 = require("../utils/dataProcessing");
 function configureSockets(io) {
     let dataHistory = [];
     let record = [];
@@ -11,18 +12,19 @@ function configureSockets(io) {
     //criar uma função para gerar cordenadas em circulo  
     const cordenadas = (0, setCoordenates_1.setCoordenate)();
     let coord = 0;
+    let intervalo = setInterval(function () {
+        if (coord === 100) {
+            coord = 0;
+        }
+        coord++;
+        let newData = (0, dataProcessing_1.convertData)(cordenadas, coord);
+        io.emit("info", newData);
+    }, 1000);
     io.on('connection', (socket) => {
         console.log('new user: ', socket.id);
-        let intervalo = setInterval(function () {
-            if (coord === 100) {
-                coord = 0;
-            }
-            (0, boat_handlers_1.handleBoat)(socket, io, speed, cordenadas, coord);
-            (0, history_handlers_1.handleRaceHistory)(socket, io, dataHistory, record, speed);
-            coord++;
-        }, 1000);
+        (0, boat_handlers_1.handleBoat)(socket, io, speed, cordenadas);
+        (0, history_handlers_1.handleRaceHistory)(socket, io, dataHistory, record, speed);
         socket.on('disconnect', () => {
-            clearInterval(intervalo);
             console.log('user disconnected: ', socket.id);
         });
     });
